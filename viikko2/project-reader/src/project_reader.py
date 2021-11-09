ba42cc4291b05480f620fbd7bdb7920ad26f6c1e
Erika Horros-Wallin <erika.horros@gmail.com>
2021-11-09 22:05:22 +0200

    Add projectreaderwith depencies after part 2

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,20 @@
+from urllib import request
+from project import Project
+import toml
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        parsed_toml = toml.loads(content)
+
+        name = parsed_toml['tool']['poetry']['name']
+        description = parsed_toml['tool']['poetry']['description']
+        dependencies = parsed_toml['tool']['poetry']['dependencies']
+        dev_dependencies = parsed_toml['tool']['poetry']['dev-dependencies']
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(name, description, dependencies, dev_dependencies)
