3773d7c7f8b490e5a75f36bb2f70d321470f3a9e
Erika Horros-Wallin <erika.horros@gmail.com>
2021-11-28 23:21:00 +0200

    Ostokori toimii kun lisataan kaksi samaa tavaraa

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -7,9 +7,9 @@ class Ostoskori:
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        maara = 0
-        for o in self._ostoskori:
-            maara+=1
+        maara=0
+        for t in self._ostoskori:
+            maara += t.lukumaara() 
         return maara
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
@@ -23,6 +23,11 @@ class Ostoskori:
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
+        for o in self._ostoskori:
+            if o.tuotteen_nimi() == lisattava.nimi():
+                o.muuta_lukumaaraa(1)
+                return
+
         self._ostoskori.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
