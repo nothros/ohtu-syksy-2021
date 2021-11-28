574f4fb8e5a7ff5cc8b6a073b7ce5ca2df3b42f2
Erika Horros-Wallin <erika.horros@gmail.com>
2021-11-28 22:46:49 +0200

    Ostoskori palauttaa oikean hinnan tuotteen lisäämisen jälkeen

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -16,7 +16,10 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        yht_hinta = 0
+        for o in self._ostoskori:
+            yht_hinta+=o.hinta()
+        return yht_hinta
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
